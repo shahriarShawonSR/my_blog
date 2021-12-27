36904cf897d1f9f3da73d04cb317652a183cba62
shahriarshawon01 <shahriarshawon001@gmail.com>
2021-12-28 02:09:00 +0600

    add auth

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
+// Admin Route
+Route::group(['as' => 'admin.', 'prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth', 'admin']], function () {
+
+    Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+});
+// Author Route
+Route::group(['as' => 'author.', 'prefix' => 'author', 'namespace' => 'Author', 'middleware' => ['auth', 'author']], function () {
+
+    Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+});
